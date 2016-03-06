c83032b65cd0326d1aa9a6ca37ec91e4ab1b2b76
CoderLiu <337451700@qq.com>
2016-03-06 18:05:06 +0800

    修复颜色混乱问题
    添加切换页面颜色渐变效果

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # FEcoders.github.io
+
 [百度前端技术学院FEcoders小队](http://FEcoders.github.io)
--- js/main.js
+++ js/main.js
@@ -1,12 +1,35 @@
 
 $(document).ready(function() {
+
+    //配置
+    var configure = {
+        color : ["#1BBC9B","#BFDA00","#2C3E50","#FF9900"]  //每屏的颜色
+    };
+
     $('#fullpage').fullpage({
-        continuousVertical: true,  //循环演示
-        //绑定菜单
+        //循环演示
+        continuousVertical: true,
+        //每屏的颜色
+        sectionsColor: [configure.color[0],configure.color[1],configure.color[2],configure.color[3]],
+        //绑定菜单0
         anchors: ['page1', 'page2', 'page3', 'page4'],
         menu: '#menu',
-
         // 导航
         'navigation': true,
+        //nextIndex 是滚动到的“页面”的序号，从1开始计算；direction 判断往上滚动还是往下滚动，值是 up 或 down。
+        onLeave: function(index,nextIndex,direction){
+            for(var i=0;i<4;i++){
+                if(i == nextIndex-1){
+                    colorChange(configure.color[i]);
+                    break;
+                }
+            }
+        }
     });
+    //改变颜色
+    function colorChange(col){
+        $('.section').animate({
+            backgroundColor: col
+        }, 700);
+    }
 });
