2c18f1e2215ebe9167329e3925bbad1e6be3cb5c
CoderLiu <337451700@qq.com>
2016-03-07 12:27:34 +0800

    优化动画效果，箭头添加锚点链接

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 
 $(document).ready(function() {
-
     //配置
     var configure = {
         color : ["#1BBC9B","#BFDA00","#2C3E50","#FF9900"], //每屏的颜色
@@ -8,8 +7,10 @@ $(document).ready(function() {
     var arrow = $("#next");
     //获取屏幕宽度
     var sWidth = $(window).width();
-    console.log(sWidth);
-    var content1Left = $(".content1-left"),
+    var next = $("#next"),
+        FEC1 = $(".section1 .FEC1"),
+        FEC2 = $(".section1 .FEC2"),
+        content1Left = $(".content1-left"),
         content1Right = $(".content1-right"),
         content1LeftF = $(".content1-left-F0"),
         content1LeftE = $(".content1-left-E0"),
@@ -19,16 +20,58 @@ $(document).ready(function() {
         content1RightE = $(".content1-right-e0"),
         content1RightR = $(".content1-right-r0"),
         content1RightS = $(".content1-right-s0");
-    content1RightD.click(function(){
-        alert("sss")
-    })
     //初始化隐藏文字
     hideCon();
+    setTimeout(delayFEC2,2000);
+    clickNext();
+    //函数部分
+    //初始化隐藏文字
     function hideCon(){
         content1Left.css("left",-sWidth/2+"px");
-        content1Right.css("right",-sWidth/2+"px");
+        content1Right.css("right",-sWidth/2-100+"px");
     }
-
+    //移除第一屏文字动画
+    function removeClass(){
+        content1LeftF.removeClass("content1-left-F");
+        content1LeftE.removeClass("content1-left-E");
+        content1RightC.removeClass("content1-right-c");
+        content1RightO.removeClass("content1-right-o");
+        content1RightD.removeClass("content1-right-d");
+        content1RightE.removeClass("content1-right-e");
+        content1RightR.removeClass("content1-right-r");
+        content1RightS.removeClass("content1-right-s");
+    }
+    //添加第一屏文字动画
+    function addClass(){
+        content1LeftF.addClass("content1-left-F");
+        content1LeftE.addClass("content1-left-E");
+        content1RightC.addClass("content1-right-c");
+        content1RightO.addClass("content1-right-o");
+        content1RightD.addClass("content1-right-d");
+        content1RightE.addClass("content1-right-e");
+        content1RightR.addClass("content1-right-r");
+        content1RightS.addClass("content1-right-s");
+    }
+    //设置小标题延时出现
+    function delayFEC2(){
+        FEC2.css("display","block")
+    }
+    //隐藏小标题
+    function hideFEC2(){
+        FEC2.css("display","none")
+    }
+    //点击箭头，转到下一页
+    function clickNext(){
+        next.click(function(){
+            var page = window.location.hash;
+            if(page == ""){
+                page = "#page1"
+            }
+            var nextPage = parseInt(page.replace(/[^0-9]/ig,""))+1;
+            console.log(nextPage)
+            location.href="#page"+nextPage;//正则当前锚点数字，点击链接下一锚点
+        })
+    }
     $('#fullpage').fullpage({
         //循环演示
         continuousVertical: true,
@@ -54,24 +97,12 @@ $(document).ready(function() {
                 arrow.css("display","block")
             }
             if(index == 1){
-                content1LeftF.removeClass("content1-left-F");
-                content1LeftE.removeClass("content1-left-E");
-                content1RightC.removeClass("content1-right-c");
-                content1RightO.removeClass("content1-right-o");
-                content1RightD.removeClass("content1-right-d");
-                content1RightE.removeClass("content1-right-e");
-                content1RightR.removeClass("content1-right-r");
-                content1RightS.removeClass("content1-right-s");
+                setTimeout(removeClass,700);
+                setTimeout(hideFEC2,700);
             }
             if(nextIndex == 1){
-                content1LeftF.addClass("content1-left-F");
-                content1LeftE.addClass("content1-left-E");
-                content1RightC.addClass("content1-right-c");
-                content1RightO.addClass("content1-right-o");
-                content1RightD.addClass("content1-right-d");
-                content1RightE.addClass("content1-right-e");
-                content1RightR.addClass("content1-right-r");
-                content1RightS.addClass("content1-right-s");
+                addClass();
+                setTimeout(delayFEC2,2000);
             }
         }
     });
