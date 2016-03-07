ffd368212a3cfe132c11dbf5311a4171ab4805d3
马世祥 <2223673415@qq.com>
2016-03-07 12:39:45 +0800

    about page framework

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 
     //配置
     var configure = {
-        color : ["#1BBC9B","#BFDA00","#2C3E50","#FF9900"]  //每屏的颜色
+        color : ["#1BBC9B","#4BBFC3","#2C3E50","#FF9900"]  //每屏的颜色
     };
 
     $('#fullpage').fullpage({
@@ -14,8 +14,7 @@ $(document).ready(function() {
         //绑定菜单0
         anchors: ['page1', 'page2', 'page3', 'page4'],
         menu: '#menu',
-        // 导航
-        'navigation': true,
+        
         //nextIndex 是滚动到的“页面”的序号，从1开始计算；direction 判断往上滚动还是往下滚动，值是 up 或 down。
         onLeave: function(index,nextIndex,direction){
             for(var i=0;i<4;i++){
@@ -23,6 +22,14 @@ $(document).ready(function() {
                     colorChange(configure.color[i]);
                     break;
                 }
+            }
+        },
+
+        afterSlideLoad: function( anchorLink, index, slideAnchor, slideIndex){
+            if(anchorLink == 'page2'){
+                $(".about img").animate({
+                    top: "10%"
+                }, 700);
             }
         }
     });
